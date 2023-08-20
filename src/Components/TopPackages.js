24630d84e25c9f44157caf55896e2277a3ad0612
Nandhagopal011 <nanthagopal9384@gmail.com>
2023-08-20 18:52:20 +0530

    first commit

--- src/Components/TopPackages.js
+++ src/Components/TopPackages.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Placeholder from 'react-bootstrap/Placeholder';
+import Placeholder from 'react-bootstrap';
     
 
 export const TopPackages = () => {
